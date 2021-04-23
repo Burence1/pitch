579ee17e5f7c4afc56d23078e521a90c4a352d02
Burence1 <burensomondi@gmail.com>
2021-04-23 13:14:00 +0300

    migrate pitches table to db

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
+from datetime import datetime
 
 class User(UserMixin,db.Model):
   __tablename__ = 'users'
@@ -11,3 +12,13 @@ class User(UserMixin,db.Model):
   bio = db.Column(db.String(255))
   password_hash = db.Column(db.String(255))
   profile_path = db.Column(db.String(255))
+  pitches = db.relationship('Pitch',backref='user',lazy="dynamic")
+
+class Pitch(db.Model):
+  __tablename__ = 'pitches'
+
+  id = db.Column(db.Integer,primary_key=True)
+  title = db.Column(db.String(255))
+  category = db.Column(db.String(255))
+  posted = db.Column(db.DateTime,default=datetime.utcnow)
+  user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
