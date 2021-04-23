d07152a9af406d2cc81e00ec13d75cba4c9fc727
Burence1 <burensomondi@gmail.com>
2021-04-23 13:49:47 +0300

    make migrations for table relationships

--- app/models.py
+++ app/models.py
@@ -13,6 +13,9 @@ class User(UserMixin,db.Model):
   password_hash = db.Column(db.String(255))
   profile_path = db.Column(db.String(255))
   pitches = db.relationship('Pitch',backref='user',lazy="dynamic")
+  comments = db.relationship('Comment',backref='user',lazy="dynamic")
+  upvotes = db.relationship('Upvote', backref='user', lazy="dynamic")
+  downvotes = db.relationship('Downvote',backref='user',lazy="dynamic")
 
 class Pitch(db.Model):
   __tablename__ = 'pitches'
@@ -22,3 +25,31 @@ class Pitch(db.Model):
   category = db.Column(db.String(255))
   posted = db.Column(db.DateTime,default=datetime.utcnow)
   user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+  comments = db.relationship('Comment',backref='pitch',lazy="dynamic")
+  upvotes = db.relationship('Upvote',backref='pitch',lazy="dynamic")
+  downvotes=db.relationship('Downvote',backref='pitch',lazy="dynamic")
+
+class Comment(db.Model):
+  __tablename__='comments'
+
+  id = db.Column(db.Integer,primary_key=True)
+  contents = db.Column(db.Integer)
+  pitch_id = db.Column(db.Integer,db.ForeignKey("pitches.id"))
+  user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+class Upvote(db.Model):
+  __tablename__='upvotes'
+
+  id=db.Column(db.Integer,primary_key=True)
+  counter=db.Column(db.Integer)
+  user_id=db.Column(db.Integer,db.ForeignKey("users.id"))
+  pitch_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
+
+
+class Downvote(db.Model):
+  __tablename__='downvotes'
+
+  id=db.Column(db.Integer,primary_key=True)
+  counter = db.Column(db.Integer)
+  user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+  pitch_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
