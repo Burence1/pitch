d725e642f972bcf4f7f24b1cbc15459218733013
Burence1 <burensomondi@gmail.com>
2021-04-26 16:33:00 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY='5e83ea13-f3d9-4e93-b17e-30cdacc5b26c'
 export MAIL_USERNAME='burensdev@gmail.com'
 export MAIL_PASSWORD='Burensdev1.'
-export DATABASE_URL='postgresql://lrmzkbwrkutbhs:fab533cb4e50dd869d745ca6e82c30971727e6acf1bb30bb19d19afde057853a@ec2-54-224-120-186.compute-1.amazonaws.com:5432/dc1lg4ch2jobme?sslmode=require'
+export DATABASE_URL='postgresql://laggsyziustxcr:849e39929fd6c8233667348b161f109017b503c7c2d49ff179241ed4da6d420f@ec2-54-224-120-186.compute-1.amazonaws.com:5432/ddc5aa278ru9nf?sslmode=require'
 
 python3.6 manage.py server
