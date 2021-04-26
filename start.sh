8095c45bec30993cbaa4cf26f82c34a9fdd38c22
Burence1 <burensomondi@gmail.com>
2021-04-26 15:37:10 +0300

    fix bug

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export SECRET_KEY='5e83ea13-f3d9-4e93-b17e-30cdacc5b26c'
 export MAIL_USERNAME='burensdev@gmail.com'
 export MAIL_PASSWORD='Burensdev1.'
+export DATABASE_URL='postgresql://lrmzkbwrkutbhs:fab533cb4e50dd869d745ca6e82c30971727e6acf1bb30bb19d19afde057853a@ec2-54-224-120-186.compute-1.amazonaws.com:5432/dc1lg4ch2jobme?sslmode=require'
 
 python3.6 manage.py server
