80d1d5359dcb3539e303bdc6ddca6ce66590bb95
Burence1 <burensomondi@gmail.com>
2021-04-28 10:24:34 +0300

    deploy

--- app/models.py
+++ app/models.py
@@ -60,7 +60,7 @@ class Comment(db.Model):
   __tablename__='comments'
 
   id = db.Column(db.Integer,primary_key=True)
-  comment = db.Column(db.String(),nullable=True)
+  comment = db.Column(db.Text(),nullable=True)
   posted = db.Column(db.DateTime,default=datetime.utcnow)
   pitch_id = db.Column(db.Integer,db.ForeignKey("pitches.id"))
   user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
